70320fb9303342f9ba65f9bd0234f75126abf391
Axel Aranda <axelemilianoaranda1993@gmail.com>
2025-10-15 19:24:54 -0300

    A

--- aegir/src/app/boats/[boatId]/page.tsx
+++ aegir/src/app/boats/[boatId]/page.tsx
@@ -26,7 +26,7 @@ export default function BoatDetail() {
     <main
      className="min-h-screen bg-cover bg-center bg-no-repeat bg-fixed"
      style={{ backgroundImage: "url(/playa.jpg)" }}>
-    <article className="min-h-dvh flex items-center justify-center items-start pt-20 pb-20">
+    <article className="min-h-dvh flex items-center justify-center items-start pt-28 pb-28">
     <section className="border rounded-xl flex flex-col max-w-[900px] w-full overflow-hidden shadow-sm bg-custom-light p-4 mb-20 m-3 sm:m-6">
 
       <Carousel images={boatData.images} alt={boatData.name} className="mb-4" />
--- aegir/src/app/page.tsx
+++ aegir/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const allProducts = boats;
 
   return (
-    <main className="min-h-screen bg-cover bg-center bg-no-repeat bg-fixed !pt-28"
+    <main className="min-h-screen bg-cover bg-center bg-no-repeat bg-fixed !pt-28 !pb-28"
     style={{ backgroundImage: "url(/playa.jpg)" }}>
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-6 md:mx-10 lg:mx-20 xl:mx-28">{allProducts.map((product) => {
         return (
