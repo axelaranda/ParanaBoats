09e3ba4ca4b672622fb74c47f94249cc197c1fa7
Axel Aranda <axelemilianoaranda1993@gmail.com>
2025-10-16 01:00:52 -0300

    A

--- aegir/src/components/Footer.tsx
+++ aegir/src/components/Footer.tsx
@@ -34,8 +34,8 @@ const Footer = () => {
          <h2 className="inline-block text-custom-dark font-poppins text-center mt-6 px-2">Mi nombre es Axel Aranda, estoy en contacto con el río desde los 3 días de vida cuando me llevaban a navegar en el Plenamar 27 de mi abuelo, empecé a navegar en optimist a los 9 años, empecé a competir a los 11, pasé también por láser -ahora llamada ILCA-, navegué en numerosos veleros lastrados y también fui instructor de optimist e ILCA; podría afirmar sin lugar a duda que soy un apasionado de la náutica</h2>
 
           <div className="mx-auto w-full sm:w-fit max-w-md px-4 sm:px-2 py-6 text-center text-sm font-poppins leading-relaxed text-custom-dark mt-12 mb-12 break-words whitespace-normal">
-            <p>WhatsApp: 343 5032200</p>
-            <p className="break-all">Email: axelemilianoaranda1993@gmail.com</p>
+            <p>  <a href="https://wa.me/5493435032200" className="text-green-600 hover:text-green-300" target="_blank" rel="noopener noreferrer">WhatsApp</a></p>
+            <p className="break-all">  <a href="mailto:axelemilianoaranda1993@gmail.com" className="text-blue-600 hover:text-blue-300">axelemilianoaranda1993@gmail.com</a></p>
             <p>Localidad: Paraná, Entre Ríos, Argentina</p>
             <p>Su consulta no molesta, no hay preguntas tontas, solo tontos que no preguntan</p>
           </div>
